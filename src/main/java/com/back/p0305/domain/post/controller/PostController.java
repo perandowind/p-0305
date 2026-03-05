f11491073c78c84c13af93832889f69a075452fd
perandowind <perandowind@gmail.com>
2026-03-05 12:54:40 +0900

    1~7

--- src/main/java/com/back/p0305/domain/post/controller/PostController.java
+++ src/main/java/com/back/p0305/domain/post/controller/PostController.java
@@ -35,11 +35,36 @@ public class PostController {
     @PostMapping("/posts/write")
     @ResponseBody
     public String write(@RequestParam String title, String content) {
+        // 유효성 체크
+        if (title.isBlank()) {
+            return """
+                    <div style="color:red">제목을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteForm());
+        }
+        if (content.isBlank()) {
+            return """
+                    <div style="color:red">내용을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteForm());
+        }
 
         // GetMapping 요청이라 중복 경우 처리를 안할 수 있음(다른 개발자가 볼때)
         Post post = postService.write(title, content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
+    }
+
+    private String getWriteForm() {
+        return """
+                <form method="post" action="/posts/write">
+                  <input type="text" name="title">
+                  <br>
+                  <textarea name="content"></textarea>
+                  <br>
+                  <input type="submit" value="작성">
+                </form>
+                """;
     }
 
 }
